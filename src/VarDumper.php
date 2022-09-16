02b543a7a8211dad3326d634f5842f610ea0abf0
Muhammet ŞAFAK <info@muhammetsafak.com.tr>
2022-09-16 14:46:47 +0300

    Genel yapı oluşturuldu

---
+++ src/VarDumper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * VarDumper.php
+ * 
+ * @author     Muhammet ŞAFAK <info@muhammetsafak.com.tr>
+ * @copyright  VarDumper.php © 2022-09-16T11:40:45.910Z
+ * @version    0.1
+ * @link       https://www.muhammetsafak.com.tr
+ * @license    MIT
+ */
+
+declare(strict_types=1);
+
+namespace InitPHP\VarDumper;
+
+class VarDumper
+{
+    
+}
---
+++ src/VarDumperInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * VarDumperInterface.php
+ * 
+ * @author     Muhammet ŞAFAK <info@muhammetsafak.com.tr>
+ * @copyright  VarDumperInterface.php © 2022-09-16T11:41:58.893Z
+ * @version    0.1
+ * @link       https://www.muhammetsafak.com.tr
+ * @license    MIT
+ */
+
+declare(strict_types=1);
+
+interface VarDumperInterface
+{
+
+}
